a512062b219a0094c691dea007bc88621a7cd11e
htetmyataung <htetmyataung2027@gmail.com>
2020-05-18 23:05:12 -0400

    fork works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+extern crate nix;
+
+use nix::unistd::{fork, getpid, getppid, ForkResult};
 use std::io::{self, BufRead};
+use std::process::exit;
+use std::process::Command;
 
 fn main() {
     let stdin = io::stdin();
-    while true {
+    loop {
         let line = stdin
             .lock()
             .lines()
@@ -10,6 +15,24 @@ fn main() {
             .expect("there was no next line")
             .expect("the line could not be read");
 
-        println!("{}", line)
+        for word in line.split_whitespace() {
+            println!("word '{}'", word);
+        }
+
+        let child_pid = match fork() {
+            Ok(ForkResult::Child) => {
+                println!(
+                    "[child] I'm alive! My PID is {} and PPID is {}.",
+                    getpid(),
+                    getppid()
+                );
+                exit(0);
+            }
+
+            Ok(ForkResult::Parent { child, .. }) => {}
+            Err(err) => {
+                panic!("[main] fork() failed: {}", err);
+            }
+        };
     }
 }
